c43efab80d6c88497607989e369df32b01dbc26f
MoNRaSpGit <ju4nrsuarez@gmail.com>
2026-07-06 06:32:30 -0300

    Restore scanner product images

--- src/features/alamcen/alamcen.scanner.utils.ts
+++ src/features/alamcen/alamcen.scanner.utils.ts
@@ -51,7 +51,7 @@ export function appendProductToSale(current: SaleLine[], product: LookupProduct)
       price: product.precioVenta,
       quantity: 1,
       subtotal: product.precioVenta,
-      image: null
+      image: product.tieneImagen ? product.imagen : null
     },
     ...current
   ];
